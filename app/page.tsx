eaa7ad667e6ea15be3d8c2b0bb43a550144d599b
Mert Duzgun <duzgunnmert@gmail.com>
2024-07-29 02:33:02 +0300

    change button label

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
         />
       </div>
 
-      <Button onClick={handleClick}>Render toast</Button>
+      <Button onClick={handleClick}>Register user</Button>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
